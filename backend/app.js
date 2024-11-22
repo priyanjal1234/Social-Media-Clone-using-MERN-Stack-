69860785f8f5095bdcde2298b944ea9d882e1693
priyanjal1234 <priyanjal362@gmail.com>
2024-11-22 21:06:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://social-media-clone-using-mern-stack-7wqf.onrender.com',
     credentials: true
 }))
 
